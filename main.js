a904b75b032dab0ec02742dde98588a44ecbc0cc
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-03-16 17:30:31 +0100

    Restructure object relationships and refactor code

--- main.js
+++ main.js
@@ -1,69 +1,149 @@
-// Player Factory
-function createPlayer(symbol) {
-  let moves = 0;
-  const player = Object.create(null);
-
-  player.getMoves = function() {
-    return moves;
-  };
-
-  player.updateMoves = function() {
-    ++moves;
-  };
-
-  player.getSymbol = function() {
-    return symbol;
-  }
-
-  return player;
-}
-
-// Board Module
-const boardObj = (function() {
+// Manage internal representation of board
+const board = (function() {
+  const size = 3;
   let representation = [
-    ['x', 'o', 'o'],
-    ['', 'x', ''],
-    ['', 'o', 'x']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ];
 
-  function checkBoundsValidity(x, y) {
-    return (0 <= x && x < 3) && (0 <= y && y < 3)
+  function getRepresentation() {
+    return representation;
   }
 
-  // Return board array
-  const getRepresentation = function() {
-    return representation;
-  };
+  function checkBoundsValidity(row, col) {
+    return (0 <= row && row < size) && (0 <= col && col < size);
+  }
 
-  const getValueAt = function(x, y) {
-    if (checkBoundsValidity(x, y)) {
-      return representation[y][x];
+  function getValueAt(row, col) {
+    if (checkBoundsValidity(row, col)) {
+      return representation[row][col];
     }
   }
 
-  const setValueAt = function(x, y, value) {
-    if (checkBoundsValidity(x, y)) {
-      representation[y][x] = value;
+  function setValueAt(row, col, value) {
+    if (checkBoundsValidity(row, col) && representation[row][col] === '') {
+      representation[row][col] = value;
       return true;
     }
 
     return false;
   }
 
+  function updateCount(row, col, p1Count, p2Count) {
+    switch(representation[row][col]) {
+      case 'x': ++p1Count; break;
+      case 'o': ++p2Count; break;
+    }
+
+    return [p1Count, p2Count];
+  }
+
+  function getWinner(p1Count, p2Count) {
+    const countForWin = size;
+    if (p1Count === countForWin) {
+      return 'x';
+    } else if (p2Count === countForWin) {
+      return 'o';
+    }
+
+    return null;
+  }
+
+  function checkHorizontals() {
+    for (let col = 0; col < size; col++) {
+      let p1Count = 0;
+      let p2Count = 0;
+
+      for (let row = 0; row < size; row++) {
+        [p1Count, p2Count] = updateCount(row, col, p1Count, p2Count);
+      }
+
+      const winner = getWinner(p1Count, p2Count);
+      if (winner) {
+        return winner;
+      }
+    }
+
+    return null;
+  }
+
+  function checkVerticals() {
+    for (let row = 0; row < size; row++) {
+      let p1Count = 0;
+      let p2Count = 0;
+
+      for (let col = 0; col < size; col++) {
+        [p1Count, p2Count] = updateCount(row, col, p1Count, p2Count);
+      }
+
+      const winner = getWinner(p1Count, p2Count);
+      if (winner) {
+        return winner;
+      }
+    }
+
+    return null;
+  }
+
+  function checkDiagonals() {
+    let winner;
+    let p1Count = 0;
+    let p2Count = 0;
+
+    // Check left-diagonal
+    for (let row = 0, col = 0; row < size; row++, col++) {
+      [p1Count, p2Count] = updateCount(row, col, p1Count, p2Count);
+    }
+
+    winner = getWinner(p1Count, p2Count);
+    if (winner) {
+      return winner;
+    }
+
+    // Reset symbol count for both players before final check
+    p1Count = p2Count = 0;
+
+    // Check right-diagonal
+    for (let row = 0, col = size - 1; row < size; row++, col--) {
+      [p1Count, p2Count] = updateCount(row, col, p1Count, p2Count);
+    }
+
+    return getWinner(p1Count, p2Count);
+  }
+
+  function checkStatus() {
+    let status = '';
+    status = checkHorizontals();
+    if (status !== null) {
+      return status;
+    }
+
+    status = checkVerticals();
+    if (status !== null) {
+      return status;
+    }
+
+    status = checkDiagonals();
+    if (status !== null) {
+      return status;
+    }
+  }
+
   // Set all tiles of the board to empty string
-  const clear = function() {
-    representation.forEach((row, y) => {
-      row.forEach((tile, x) => {
-        representation[y][x] = '';
+  function clear() {
+    representation.forEach((rowValue, rowIndex) => {
+      rowValue.forEach((colValue, colIndex) => {
+        representation[rowValue][colIndex] = '';
       });
     });
   }
 
-  return {getRepresentation, getValueAt, setValueAt, clear};
+  return {getRepresentation, getValueAt, setValueAt, checkStatus, clear};
 })();
 
-// Display Module
-const displayObj = (function(boardElement, boardRepresentation) {
+// Control DOM manipulation
+const display = (function(boardElement, boardRepresentation) {
 
   // Remove the current board and create a new one
   function createNewBoardElement() {
@@ -74,32 +154,31 @@ const displayObj = (function(boardElement, boardRepresentation) {
   }
 
   function getTileIndex(tileElement) {
-    return [tileElement.dataset.x, tileElement.dataset.y];
+    return [tileElement.dataset.row, tileElement.dataset.col];
   }
 
   function handleTileElementClick(e) {
-    e.stopPropagation();
-    let [x, y] = getTileIndex(e.target);
-    gameObj.placeSymbolForPlayer(x, y);
-    render();
+    let [row, col] = getTileIndex(e.target);
+    game.placeSymbolForCurrentPlayer(row, col);
+    renderBoard();
   }
 
   // Render contents of board representation to screen
-  const render = function() {
+  function renderBoard() {
     // Recreate all DOM elements with updated values
     boardElement = createNewBoardElement();
 
-    boardRepresentation.forEach((row, y) => {
+    boardRepresentation.forEach((rowValue, rowIndex) => {
       const rowElement = document.createElement('div');
       rowElement.classList.add('row');
 
-      row.forEach((tile, x) => {
+      rowValue.forEach((colValue, colIndex) => {
         const tileElement = document.createElement('div');
         tileElement.classList.add('tile');
-        tileElement.dataset.x = x;
-        tileElement.dataset.y = y;
-        tileElement.dataset.symbol = tile;
-        tileElement.textContent = tile;
+        tileElement.dataset.row = rowIndex;
+        tileElement.dataset.col = colIndex;
+        tileElement.dataset.symbol = colValue;
+        tileElement.textContent = colValue;
         tileElement.addEventListener('click', handleTileElementClick);
         rowElement.appendChild(tileElement);
       });
@@ -108,16 +187,17 @@ const displayObj = (function(boardElement, boardRepresentation) {
     });
 
     document.body.appendChild(boardElement);
-  };
+  }
 
-  return {render};
-})(document.getElementById('board'), boardObj.getRepresentation());
+  return {renderBoard};
+})(document.getElementById('board'), board.getRepresentation());
 
-// Game Module
-const gameObj = (function() {
-  const players = [createPlayer('x'), createPlayer('o')];
+// Manage game state
+const game = (function() {
+  const arrayOfPlayers = [createPlayer('x'), createPlayer('o')];
+  let gameIsOn = false;
   let currentPlayerIndex = 0;
-  let currentPlayer = players[currentPlayerIndex];
+  let currentPlayer = arrayOfPlayers[currentPlayerIndex];
 
   function updateCurrentPlayer() {
     if (currentPlayerIndex === 1) {
@@ -126,37 +206,81 @@ const gameObj = (function() {
       currentPlayerIndex = 1;
     }
 
-    return players[currentPlayerIndex];
+    return arrayOfPlayers[currentPlayerIndex];
   }
 
-  const placeSymbolForPlayer = function(x, y) {
-    const currentTileValue = boardObj.getValueAt(x, y);
-    if (currentTileValue === '') {
-      const moveIsValid = boardObj.setValueAt(x, y, currentPlayer.getSymbol());
-      if (moveIsValid) {
-        currentPlayer.updateMoves();
-        if (currentPlayer.getMoves() >= 3) {
-          const boardStatus = boardObj.getStatus();
-          if (boardStatus === currentPlayer.getSymbol()) {
-            displayWinner();
-          } else if (boardStatus === 'draw') {
-            displayResetMessage();
-          }
+  function getCurrentPlayer() {
+    return currentPlayer;
+  }
+
+  function placeSymbolForCurrentPlayer(row, col) {
+    const moveIsValid = board.setValueAt(row, col, currentPlayer.getSymbol());
+    if (moveIsValid) {
+      currentPlayer.updateMoves();
+      if (currentPlayer.getMoves() >= 3) {
+        const boardStatus = board.checkStatus();
+        if (boardStatus === currentPlayer.getSymbol()) {
+          declareWinner(currentPlayer);
+        } else if (boardStatus === 'draw') {
+          declareDraw();
         }
-
-        currentPlayer = updateCurrentPlayer();
       }
-    }
-  };
-  
-  const reset = function() {
-    boardObj.clear();
-    displayObj.render();
-    currentPlayerIndex = 0;
-  };
 
-  return {boardObj, displayObj, placeSymbolForPlayer, reset};
+      currentPlayer = updateCurrentPlayer();
+    }
+  }
+  
+  function reset() {
+    board.clear();
+    display.renderBoard();
+  }
+
+  function declareWinner(winner) {
+    console.log(winner.getSymbol(), 'wins');
+    // display.renderWinnerMessage(winner);
+    gameIsOn = false;
+  }
+
+  function declareDraw() {
+    display.renderDrawMessage();
+    gameIsOn = false;
+  }
+
+  function play() {
+    if (!gameIsOn) {
+      gameIsOn = true;
+      display.renderBoard();
+    } else {
+      console.log('game is currently ongoing');
+    }
+  }
+
+  return {play, placeSymbolForCurrentPlayer};
 })();
 
-gameObj.displayObj.render();
-gameObj.reset();
+// Create player objects
+function createPlayer(symbol) {
+  let moves = 0;
+  const playerProto = Object.create(null, {
+    getMoves: {
+      value: function() {
+        return moves;
+      }
+    },
+    updateMoves: {
+      value: function() {
+        return ++moves;
+      }
+    },
+    getSymbol: {
+      value: function() {
+        return symbol;
+      }
+    },
+  });
+
+  return Object.create(playerProto);
+}
+
+// Initialization
+game.play();
